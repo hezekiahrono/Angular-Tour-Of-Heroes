e84834cb0f4e83e77b2584c28d66c4b842aba0e2
Hezekiah Rono <hrono@tracom.co.ke>
2019-03-25 13:09:35 +0300

    First Commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';
+
+export const  HEROES: Hero[] = [
+  {id: 11, name: 'Fridah'},
+  {id: 12, name: 'Obed'},
+  {id: 13, name: 'Nelsom'},
+  {id: 14, name: 'Hezekiah'},
+  {id: 15, name: 'Nelson'},
+  {id: 16, name: 'Maurice'}
+];
